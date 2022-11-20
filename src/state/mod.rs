95b4e7eb7e6c93be0d6fc29a647da7c5e9a752c2
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-11-20 08:35:19 +0000

    feat: ✨ (state/GameState) transition_toとnot_equalメソッドを追加

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,10 +1,33 @@
-#[derive(Debug)]
+use std::sync::Mutex;
+
+#[derive(Debug, PartialEq)]
 pub enum GameState {
     None,
     Preparing,
     Discussion,
     Voting,
     Evening,
+}
+
+impl GameState {
+    pub fn transition_to(
+        game_state: &Mutex<GameState>,
+        next_state: GameState,
+    ) -> anyhow::Result<()> {
+        let Ok(mut game_state) = game_state.lock() else {
+            anyhow::bail!("Failed to lock game state");
+        };
+        *game_state = next_state;
+
+        Ok(())
+    }
+
+    pub fn not_equal(game_state: &Mutex<GameState>, comparison: GameState) -> anyhow::Result<bool> {
+        let Ok(game_state) = game_state.lock() else {
+            anyhow::bail!("Failed to lock game state");
+        };
+        Ok(*game_state != comparison)
+    }
 }
 
 impl Default for GameState {
