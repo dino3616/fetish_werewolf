74e7ad03d17b0a02a18ad792e8d07c1eb51d505f
dino3616 <85730998+dino3616@users.noreply.github.com>
2022-11-20 08:34:00 +0000

    feat: ✨ (error) anyhow::Errorもハンドリングできるように修正

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,23 +1,44 @@
-use poise::serenity_prelude as serenity;
+use poise::{serenity_prelude as serenity, FrameworkError::*};
+use std::fmt::Display;
 
-use crate::context::Data;
+use crate::context::{Context, Data};
 
 pub async fn on_error(error: poise::FrameworkError<'_, Data, anyhow::Error>) {
     log::error!("Error: {:?}", error);
 
-    if let Some(ctx) = error.ctx() {
-        let result = ctx
-            .send(|rep| {
-                rep.embed(|emb| {
-                    emb.title("Oops! something went wrong! :-(")
-                        .description(format!("```{}```", error))
-                        .color(serenity::Color::RED)
-                })
-                .ephemeral(true)
-            })
-            .await;
-        if let Err(err) = result {
-            log::error!("Error occurred when sending error message: {:?}", err);
+    let result = match error {
+        Command { error, ctx, .. } => reply_error(ctx, error).await,
+        _ => {
+            if let Some(ctx) = error.ctx() {
+                reply_error(ctx, error).await
+            } else {
+                Ok(())
+            }
         }
+    };
+
+    if let Err(err) = result {
+        log::error!("Error occurred when sending error message: {:?}", err);
     }
+}
+
+async fn reply_error<'a>(ctx: Context<'a>, error: impl Display) -> anyhow::Result<()> {
+    let error = if error.to_string().ends_with('.') {
+        error.to_string()
+    } else {
+        format!("{}.", error)
+    };
+
+    ctx.send(|rep| {
+        rep.embed(|emb| {
+            emb.title("Oops! something went wrong! :-(")
+                .description(format!("```{}```", error))
+                .color(serenity::Color::RED)
+        })
+        .ephemeral(true)
+    })
+    .await
+    .map(|_| ())?;
+
+    Ok(())
 }
